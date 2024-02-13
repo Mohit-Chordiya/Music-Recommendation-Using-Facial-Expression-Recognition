481e42ddcc1449bb601f07126cf9d708c1e0d761
Mohit-Chordiya <mohitmchordiya33@gmail.com>
2024-02-13 18:53:21 +0530

    added build_files.sh

--- music/settings.py
+++ music/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-y1x_bz(tqo_hk@aip+qh$!4i@y5m4-d&8o_i02#chucvd$nbuv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".vercel.app"]
+ALLOWED_HOSTS = [".vercel.app", ".now.sh", "127.0.0.1", "localhost"]
 
 
 # Application definition
@@ -117,13 +117,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
-STATICFILES_DIRS = [
-    BASE_DIR, "static"
-]
--- music/urls.py
+++ music/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from music import views
+from music import settings
+import static
 
 urlpatterns = [
     path('admin-panel/', admin.site.urls),
@@ -28,3 +30,4 @@ urlpatterns = [
     path('spotify/',views.spotifyApi,name='spotifyApi'),
     path('stop_streaming/', views.stop_streaming, name='stop_streaming'),
 ]
+
